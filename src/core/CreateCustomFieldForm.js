5f7064351e0451d32fa193ee9d43fcea99bf3fa4
Manolis <emylonidis@gmail.com>
2013-09-24 10:20:01 +0300

    Various fixes

--- src/core/CreateCustomFieldForm.js
+++ src/core/CreateCustomFieldForm.js
@@ -6,16 +6,21 @@ var CreateCustomFieldForm = function(name, mailingListId, customFieldType, optio
   this.CustomFieldType = customFieldType;
 };
 
-CreateCustomFieldForm.prototype.validate = function() {
-  var validation = { valid: true, errors: [] };
-  if (!this.Name) { 
-    validation.valid = false;
-    validation.errors.push('Name is a required parameter for CreateCustomFieldForm object');
-  }
-  if (!this.MailingListId) { 
-    validation.valid = false;
-    validation.errors.push('MailingListId is a required parameter for CreateCustomFieldForm object');
-  }
-
-  return validation;
+CreateCustomFieldForm.prototype.validate = function () {
+    var validation = { valid: true, errors: [] };
+    if (!this.Name) {
+        validation.valid = false;
+        validation.errors.push('Name is a required parameter for CreateCustomFieldForm object');
+    }
+    if (!this.MailingListId) {
+        validation.valid = false;
+        validation.errors.push('MailingListId is a required parameter for CreateCustomFieldForm object');
+    }
+    if (this.CustomFieldType == 3) {
+        if (!this.Options || this.Options.length == 0) {
+            validation.valid = false;
+            validation.errors.push('CreateCustomFieldForm must have some options when CustomFieldType == 3');
+        }
+    }
+    return validation;
 };
--- src/core/UpdateCustomFieldForm.js
+++ src/core/UpdateCustomFieldForm.js
@@ -1,5 +1,5 @@
 var UpdateCustomFieldForm = function(id, name, mailingListId, customFieldType, options, isRequired) {
-	this.Id = id;
+  this.Id = id;
   this.Name = name;
   this.Options = options;
   this.IsRequired = isRequired;
@@ -7,16 +7,21 @@ var UpdateCustomFieldForm = function(id, name, mailingListId, customFieldType, o
   this.CustomFieldType = customFieldType;
 };
 
-UpdateCustomFieldForm.prototype.validate = function() {
-  var validation = { valid: true, errors: [] };
-  if (!this.Id) { 
-    validation.valid = false;
-    validation.errors.push('Id is a required parameter for UpdateCustomFieldForm object');
-  }
-  if (!this.MailingListID) { 
-    validation.valid = false;
-    validation.errors.push('MailingListID is a required parameter for UpdateCustomFieldForm object');
-  }
-
-  return validation;
+UpdateCustomFieldForm.prototype.validate = function () {
+    var validation = { valid: true, errors: [] };
+    if (!this.Id) {
+        validation.valid = false;
+        validation.errors.push('Id is a required parameter for UpdateCustomFieldForm object');
+    }
+    if (!this.MailingListId) {
+        validation.valid = false;
+        validation.errors.push('MailingListID is a required parameter for UpdateCustomFieldForm object');
+    }
+    if (this.CustomFieldType == 3) {
+        if (!this.Options || this.Options.length == 0) {
+            validation.valid = false;
+            validation.errors.push('UpdateCustomFieldForm must have some options when CustomFieldType == 3');
+        }
+    }
+    return validation;
 };
